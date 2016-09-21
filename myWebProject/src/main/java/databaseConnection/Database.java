3d7cda61e5d1ae8e1a3d1ebc7e7afc23be33ee54
lanedarx <lanedarx@gmail.com>
2016-09-21 08:01:24 +0300

    Добавлен класс для соединения с базой данным и драйвер

--- myWebProject/src/main/java/databaseConnection/Database.java
+++ myWebProject/src/main/java/databaseConnection/Database.java
@@ -1,4 +1,13 @@
 package databaseConnection;
 
+import java.sql.Connection;
+
 public class Database {
+    String url = "jdbc:postgresql://127.0.0.1:5432/musicdb";
+    String name = "postgres";
+    String password = "TcnmLjcneg505";
+    Connection connection = null;
+
+    Class.forName("org.postgresql.Driver");
+    System.out.println("Драйвер подключен");
 }
